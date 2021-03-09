a6b0075b50a0576348602ac6ebfa29263c0a8aaf
popples <klauspopp@gmx.de>
2021-03-09 15:39:02 +0100

    Update robot-run.sh

--- robot-run.sh
+++ robot-run.sh
@@ -22,4 +22,4 @@ trap finish EXIT
 (env | grep ^R[OE]BOT_.*= > ${env_file} ) || :
 
 image=ci4rail/robot:latest
-docker run -v=`pwd`:/opt/robotframework/tests:Z --env-file ${env_file} -u `id -u` -a-t ${image} "${@}"
+docker run -v=`pwd`:/opt/robotframework/tests:Z --env-file ${env_file} -u `id -u` -t ${image} "${@}"

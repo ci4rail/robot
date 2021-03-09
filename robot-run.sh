ee53f2044dc26728425fb26c4bfc705c7f39f30c
popples <klauspopp@gmx.de>
2021-03-09 15:28:07 +0100

    robot-run.sh: pass local user to robot

--- robot-run.sh
+++ robot-run.sh
@@ -7,6 +7,9 @@
 set -o errexit   # abort on nonzero exitstatus
 set -o pipefail  # don't hide errors within pipes
 
+# pass local user to robot
+export ROBOT_TC_USER=${USER}
+
 # pass local IP address to robot
 export ROBOT_TC_IP=$(/sbin/ifconfig eth0 | grep -i mask | awk '{print $2}'| cut -f2 -d:)
 
